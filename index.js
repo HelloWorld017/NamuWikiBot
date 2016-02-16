49bc8b220d76cca1a3ed7dce284db586f8e939ab
Khinenw <deu07115@gmail.com>
2016-02-16 12:41:45 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var chalk = require('chalk');
 var request = require('request');
 var util = require('util');
 
-var telegram = require('./telegram-bot');
+var telegram = require('telegram-bot');
 var remover = require('./remover.js');
 var config = require('./config/');
 
